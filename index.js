719cbd45e6bf09cf8feb824fe3948e7a7257873a
AjiroAlex <167733502+AjiroAlex@users.noreply.github.com>
2025-01-03 20:22:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,40 +6,52 @@ const logger = require("./utils/log");
 const express = require("express");
 
 const app = express();
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 3000;
+
+
+global.countRestart = 0;
+
+
+function startBot(message = "Starting the bot...") {
+  logger(message, "[ Starting ]");
+
+  const child = spawn(
+    "node",
+    ["--trace-warnings", "--async-stack-traces", "main.js"],
+    {
+      cwd: __dirname,
+      stdio: "inherit",
+      shell: true,
+    }
+  );
+
+  child.on("close", (codeExit) => {
+    if (codeExit !== 0 && global.countRestart < 5) {
+      global.countRestart += 1;
+      logger("Restarting bot...", "[ Restart ]");
+      startBot();
+    } else {
+      logger("Bot exited successfully or restart limit reached.", "[ Exit ]");
+    }
+  });
+
+  child.on("error", (error) => {
+    logger("An error occurred: " + JSON.stringify(error), "[ Error ]");
+  });
+}
+
+
 app.get("/", (req, res) => {
   res.send(`𝐃𝐄𝐕𝐄𝐋𝐎𝐏𝐄𝐑 𝐀𝐘𝐎𝐔𝐁`);
 });
 
-function startBot(message) {
-  (message) ? logger(message, "[ Starting ]") : "";
-
-  const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "main.js"], {
-    cwd: __dirname,
-    stdio: "inherit",
-    shell: true
-  });
-
-  child.on("close", (codeExit) => {
-    if (codeExit != 0 || global.countRestart && global.countRestart < 5) {
-      startBot("Starting up...");
-      global.countRestart += 1;
-      return;
-    } else return;
-  });
-
-  child.on("error", function(error) {
-    logger("An error occurred: " + JSON.stringify(error), "[ Starting ]");
-  });
-};
-
-
-  logger('••¤(`×[¤ AYOUB ¤]×´)¤••', "[ NAME ]");
-  logger("Version: 1.1.0", "[ VERSION ]");
-
-
-startBot();
 
 app.listen(port, () => {
-  console.log(`bot running in port: ${port}`);
+  console.log(`running on port: ${port}`);
 });
+
+
+logger("༺༽ 𝑨𝒀𝑶𝑼𝑩 𝑴𝑶𝑫𝑰𝑭𝑰𝑬𝑫༼༻", "[ NAME ]");
+logger("Version: 1.1.0", "[ VERSION ]");
+
+startBot();
